3aff52c098335eee66ecdf50912c94ab0e6bec96
Joerg Henning <henning.joerg@gmail.com>
2017-09-14 15:48:17 +0800

    Local provisioner

--- getting-started/ec2.tf
+++ getting-started/ec2.tf
@@ -5,14 +5,13 @@ provider "aws" {
 resource "aws_instance" "jx-example" {
     ami = "ami-6f198a0c"
     instance_type = "t2.micro"
+
+    provisioner "local-exec" {
+        command = "echo ${aws_instance.jx-example.public_ip} > ip_address.txt"
+    }
 }
 
 resource "aws_eip" "ip" {
     instance = "${aws_instance.jx-example.id}"
     depends_on = ["aws_instance.jx-example"]
-}
-
-resource "aws_instance" "jx-other" {
-    ami = "ami-6f198a0c"
-    instance_type = "t2.micro"
 }
