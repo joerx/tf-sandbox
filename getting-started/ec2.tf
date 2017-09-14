888584e2d95ded541cc8a98f20c9bc61ddcc181b
Joerg Henning <henning.joerg@gmail.com>
2017-09-14 16:43:17 +0800

    use output

--- getting-started/ec2.tf
+++ getting-started/ec2.tf
@@ -2,19 +2,23 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
-resource "aws_instance" "jx-example" {
+resource "aws_instance" "example" {
     ami = "${lookup(var.aws_ami_ids, var.aws_region)}"
     instance_type = "t2.micro"
 
     provisioner "local-exec" {
-        command = "echo ${aws_instance.jx-example.public_ip} > ip_address.txt"
+        command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
     }
 }
 
 # note that running the same config against different regions will not work as expected:
 # for some reason TF will not create a new eip for the different region but try to modify the
 # existing one, which will last forever.
-resource "aws_eip" "ip" {
-    instance = "${aws_instance.jx-example.id}"
-    depends_on = ["aws_instance.jx-example"]
+resource "aws_eip" "eip" {
+    instance = "${aws_instance.example.id}"
+    depends_on = ["aws_instance.example"]
+}
+
+output "ip" {
+    value = "${aws_eip.eip.public_ip}"
 }
