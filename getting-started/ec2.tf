190a2b48c4ac9de62a8c6be100f5c3d12f8652ac
Joerg Henning <henning.joerg@gmail.com>
2017-09-14 15:31:30 +0800

    Add elastic ip

--- getting-started/ec2.tf
+++ getting-started/ec2.tf
@@ -6,3 +6,13 @@ resource "aws_instance" "jx-example" {
     ami = "ami-6f198a0c"
     instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+    instance = "${aws_instance.jx-example.id}"
+    depends_on = ["aws_instance.jx-example"]
+}
+
+resource "aws_instance" "jx-other" {
+    ami = "ami-6f198a0c"
+    instance_type = "t2.micro"
+}
