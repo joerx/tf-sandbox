9363e75c1ad3fa66ebfdf9ec3fec1ec060686599
Joerg Henning <henning.joerg@gmail.com>
2017-09-14 16:35:48 +0800

    use a map for region-specific ami ids

--- getting-started/ec2.tf
+++ getting-started/ec2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "jx-example" {
-    ami = "ami-6f198a0c"
+    ami = "${lookup(var.aws_ami_ids, var.aws_region)}"
     instance_type = "t2.micro"
 
     provisioner "local-exec" {
@@ -11,6 +11,9 @@ resource "aws_instance" "jx-example" {
     }
 }
 
+# note that running the same config against different regions will not work as expected:
+# for some reason TF will not create a new eip for the different region but try to modify the
+# existing one, which will last forever.
 resource "aws_eip" "ip" {
     instance = "${aws_instance.jx-example.id}"
     depends_on = ["aws_instance.jx-example"]
--- getting-started/variables.tf
+++ getting-started/variables.tf
@@ -1,3 +1,13 @@
 variable "aws_region" {
     default = "us-east-1"
 }
+
+# AMI ids for Ubuntu Server 16.04
+variable "aws_ami_ids" {
+    type = "map"
+    default = {
+        "us-east-1" = "ami-cd0f5cb6",
+        "eu-west-1" = "ami-ebd02392",
+        "ap-southeast-1" = "ami-fdb8229e"
+    }
+}
