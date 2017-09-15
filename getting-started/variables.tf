83e093c088f755167ff6b6441a0b51195689e380
Joerg Henning <henning.joerg@gmail.com>
2017-09-15 09:49:09 +0800

    set backend for getting started, update ami id

--- getting-started/variables.tf
+++ getting-started/variables.tf
@@ -8,6 +8,7 @@ variable "aws_ami_ids" {
     default = {
         "us-east-1" = "ami-cd0f5cb6",
         "eu-west-1" = "ami-ebd02392",
-        "ap-southeast-1" = "ami-fdb8229e"
+        # "ap-southeast-1" = "ami-fdb8229e"
+        "ap-southeast-1" = "ami-6f198a0c" # amazon linux
     }
 }
